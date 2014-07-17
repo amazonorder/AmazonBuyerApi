d6690843f570b4f63b8676fd1acd8d844fb05ff3
EXTRAHD7KK <naclo3samuel@gmail.com>
2014-07-17 16:09:44 +0100

    Create ExtendedWebClient.cs
    Use this for the CookieAwareWebClient class in AmazonCls

---
+++ ExtendedWebClient.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+
+namespace MyAmazon_API
+{
+    public class CookieAwareWebClient : WebClient
+    {
+        public string MPage = "";
+        public CookieContainer CookieContainer { get; set; }
+        public Uri Uri { get; set; }
+
+        public CookieAwareWebClient()
+            : this(new CookieContainer())
+        {
+        }
+
+        public CookieAwareWebClient(CookieContainer cookies)
+        {
+            this.CookieContainer = cookies;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                (request as HttpWebRequest).CookieContainer = this.CookieContainer;
+            }
+            HttpWebRequest httpRequest = (HttpWebRequest)request;
+            httpRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            return httpRequest;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+            String setCookieHeader = response.Headers[HttpResponseHeader.SetCookie];
+
+            if (setCookieHeader != null)
+            {
+                //do something if needed to parse out the cookie.
+                if (setCookieHeader != null)
+                {
+                    Cookie cookie = new Cookie(); //create cookie
+                    if (cookie.Domain == "") goto ddd;
+                    this.CookieContainer.Add(cookie);
+                }
+            ddd: ;
+            }
+            return response;
+
+        }
+    }
+}
